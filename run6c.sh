e58e588087cbd22022ae007cb64a1c11662ce1cd
Denis Garsh <d.garsh@arenadata.io>
2025-02-03 11:45:02 +0300

    update

--- run6c.sh
+++ run6c.sh
@@ -4,24 +4,24 @@ docker network create --attachable --ipv6 --subnet 2001:db8::/112 --opt com.dock
 docker volume create gpdb
 docker stop gpdb6c || echo $?
 docker rm gpdb6c || echo $?
-mkdir -p /tmpfs/data/6c /tmpfs/data/6c.test
+mkdir -p /tmp/data/6c /tmp/data/6c.test
 docker run \
     --detach \
     --env GOPATH=/usr/local/go \
     --env GP_MAJOR=6c \
     --env GROUP_ID="$(id -g)" \
-    --env LANG=ru_RU.UTF-8 \
+    --env LANG=en_US.UTF-8 \
     --env PGPORT=6000 \
     --env PORT_BASE=6000 \
-    --env TZ=Asia/Yekaterinburg \
+    --env TZ=Europe/Moscow \
     --env USER_ID="$(id -u)" \
     --hostname gpdb6c \
     --init \
     --memory=16g \
     --memory-swap=16g \
     --mount type=bind,source="$(docker volume inspect --format "{{ .Mountpoint }}" gpdb)/.local/6c",destination=/usr/local \
-    --mount type=bind,source=/tmpfs/data/6c,destination=/home/gpadmin/.data \
-    --mount type=bind,source=/tmpfs/data/6c.test,destination=/home/gpadmin/gpdb_src/src/test \
+    --mount type=bind,source=/tmp/data/6c,destination=/home/gpadmin/.data \
+    --mount type=bind,source=/tmp/data/6c.test,destination=/home/gpadmin/gpdb_src/src/test \
     --mount type=volume,source=gpdb,destination=/home/gpadmin \
     --name gpdb6c \
     --network name=docker \
--- run6u.sh
+++ run6u.sh
@@ -1,28 +1,28 @@
 #!/bin/sh -eux
 
 docker network create --attachable --ipv6 --subnet 2001:db8::/112 --opt com.docker.network.bridge.name=docker docker || echo $?
-docker volume create gpdb
 docker stop gpdb6u || echo $?
 docker rm gpdb6u || echo $?
-mkdir -p /tmpfs/data/6u /tmpfs/data/6u.test
+mkdir -p /tmp/data/6u /tmp/data/6u.test
 docker run \
     --detach \
     --env GOPATH=/usr/local/go \
     --env GP_MAJOR=6u \
     --env GROUP_ID="$(id -g)" \
-    --env LANG=ru_RU.UTF-8 \
+    --env LANG=en_US.UTF-8 \
     --env PGPORT=6000 \
     --env PORT_BASE=6000 \
-    --env TZ=Asia/Yekaterinburg \
+    --env TZ=Europe/Moscow \
     --env USER_ID="$(id -u)" \
     --hostname gpdb6u \
     --init \
     --memory=16g \
     --memory-swap=16g \
-    --mount type=bind,source="$(docker volume inspect --format "{{ .Mountpoint }}" gpdb)/.local/6u",destination=/usr/local \
-    --mount type=bind,source=/tmpfs/data/6u,destination=/home/gpadmin/.data \
-    --mount type=bind,source=/tmpfs/data/6u.test,destination=/home/gpadmin/gpdb_src/src/test \
-    --mount type=volume,source=gpdb,destination=/home/gpadmin \
+    --mount type=bind,source=$HOME/gpdb/.local/6u,destination=/usr/local \
+    --mount type=bind,source=/tmp/data/6u,destination=/home/gpadmin/.data \
+    --mount type=bind,source=/tmp/data/6u.test,destination=/home/gpadmin/gpdb_src/src/test \
+    --mount type=bind,source=$HOME/gpdb,destination=/home/gpadmin \
+    --mount type=bind,source=$HOME/tasks,destination=/home/gpadmin/src/tasks \
     --name gpdb6u \
     --network name=docker \
     --privileged \
